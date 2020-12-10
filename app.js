224a8440b54f48340138281d1231e02b593f83c2
sumana2001 <sumana.basu2001@gmail.com>
2020-12-10 20:29:04 +0530

    initialized

---
+++ app.js
@@ -0,0 +1,14 @@
+const express=require("express");
+const bodyParser=require("body-parser");
+
+const app=express();
+
+app.set('view engine', 'ejs');
+
+app.get("/",function(req,res){
+    res.send("Hello There!");
+});
+
+app.listen(3000,function(){
+    console.log("Server started on port 3000");
+})
